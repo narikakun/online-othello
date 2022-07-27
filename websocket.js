86a27c77251e7a969997a990ede6584be88215e4
narikakun <narikakun@gmail.com>
2022-07-27 12:44:05 +0900

    Commit

--- websocket.js
+++ websocket.js
@@ -19,7 +19,7 @@ let WebSocketSettings = {
     isFinish: false,
     nickname: null,
     gameBoardLength: 8,
-    version: "202207271232"
+    version: "202207271243"
 }
 WebSocketSettings.ws.url = WebSocketSettings.ws.url + `_${WebSocketSettings.version}`;
 
